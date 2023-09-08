afdf6fdf5a3ce4a096d7aab348ae99bdd795b9ac
ACSEphvuln <72386268+ACSEphvuln@users.noreply.github.com>
2023-09-08 08:39:57 +0300

    Update puller.py
    -

--- puller.py
+++ puller.py
@@ -2,7 +2,7 @@ import PyRSS2Gen
 import datetime
 from lib.rssMaker import RSSPuller
 
-def getBreachedVC():
+def getSite1():
     filters = {
         "Title":{
             "xpath": "/td[2]/div/div[1]/span/a",
@@ -23,17 +23,17 @@ def getBreachedVC():
     }
 
     br_pull = RSSPuller(forumStruct={
-                            "url":"https://breached.vc/Forum-Databases?sortby=started&order=desc&datecut=1&prefix=0",
+                            "url":"https://example.com/db?sortby=started&order=desc&datecut=1&prefix=0",
                             "base":"/html/body/div[1]/main/table[2]/tbody",
                             "start":10,
                             "filters":filters,
                             },
-                        cachefile="cache/cache_breachedvc.txt",
+                        cachefile="cache/cache1.txt",
                         pull_type="dynamic")
     
     return br_pull.getRSSItems()
 
-def getNulledTO():
+def getSite2():
     filters = {
         "Title":{
             "xpath": "/td[2]/h4/a/span[1]",
@@ -54,20 +54,20 @@ def getNulledTO():
     }
 
     br_pull = RSSPuller(forumStruct={
-                            "url":"https://www.nulled.to/forum/184-dumps-databases/?sort_key=start_date&sort_by=Z-A",
+                            "url":"https://sub.example.com/forum/dbs",
                             "base":"/html/body/div[4]/div[3]/div/div[3]/div[4]/div[3]/div[3]/div/table",
                             "start":5,
                             "filters":filters,
                             },
-                        cachefile="cache/cache_nulledto.txt",
+                        cachefile="cache/cache2.txt",
                         pull_type="static")
 
     return br_pull.getRSSItems()
 
 def postNews(filename):
     #feed_items = []
-    #feed_items.append(getBreachedVC()) - SITE DOWN
-    feed_items = getNulledTO()
+    #feed_items.append(getSite1()) - SITE DOWN
+    feed_items = getSite2()
 
     if feed_items == []:
         print("[INF] Nothing to publish")
